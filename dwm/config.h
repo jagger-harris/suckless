c319795987469d88c87b608991672bb8fdb5a8ae
jagger-harris <jagger.lee.harris@gmail.com>
2023-11-23 14:29:08 -0600

    Fixed font issues due to recent update

--- dwm/config.h
+++ dwm/config.h
@@ -8,8 +8,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 5;       	/* vertical padding of bar */
 static const int sidepad            = 5;       	/* horizontal padding of bar */
-static const char *fonts[]          = { "SauceCodePro Nerd Font:size=12" };
-static const char dmenufont[]       = "SauceCodePro Nerd Font:size=12";
+static const char *fonts[]          = { "SauceCodePro NF:size=12" };
+static const char dmenufont[]       = "SauceCodePro NF:size=12";
 static const char col_gray1[]       = "#282c34";
 static const char col_gray2[]       = "#282c34";
 static const char col_gray3[]       = "#c5cad3";
